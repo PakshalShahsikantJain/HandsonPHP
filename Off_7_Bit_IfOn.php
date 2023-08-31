a900d0a9854889f6a08c2b74533b29bdfa3ed1a9
PakshalShashikantJain <pakshal1256@gmail.com>
2023-09-01 03:21:26 +0530

    commit

---
+++ Off_7_Bit_IfOn.php
@@ -0,0 +1,70 @@
+<?php
+    /*
+        Author : Pakshal Shashikant Jain 
+        Date : 01/09/2023
+        Program : Write a program which accept one number from user and off 7th bit of that
+                number if it is on. Return modified number. 
+    */
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    //Class Declaration
+    class OffBit
+    {
+        //Characteiristics of Class
+        public $No;
+
+        //Constructor of Class
+        public function __construct()
+        {
+            $this->No = 0;
+        }
+
+        //Behaviour of Class To Take Input from user
+        public function Accept()
+        {
+            $this->No = 73;
+        }
+
+        //Behaviour of Class To Off Bit If On
+        public function offbit()
+        {
+            $iResult = 0;
+            $iMask = 0X00000040;
+
+            $iResult = $iMask & $this->No;
+
+            if($iResult == $iMask)
+            {
+                $iResult = $iMask ^ $this->No;
+            }
+
+            return $iResult;
+        }
+    }   
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Main function
+    function main() 
+    {
+        $iRet = 0;
+
+        echo "Jay Ganesh......<br>";
+
+        //Creating object of OffBit Class
+        $cobj = new OffBit();
+
+        //Call To Behaviour of Class
+        $cobj->Accept();
+        $iRet = $cobj->offbit();
+        
+        //Printing Modified Number 
+        echo "Modified Number is : ",$iRet;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Call To Main function
+    main();
+?>
