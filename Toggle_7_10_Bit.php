a2e4926d2db888f5ad8e6083213ae7f156385c0c
PakshalShashikantJain <pakshal1256@gmail.com>
2023-09-01 12:49:39 +0530

    commit

---
+++ Toggle_7_10_Bit.php
@@ -0,0 +1,69 @@
+<?php
+    /*
+        Author : Pakshal Shashikant Jain 
+        Date : 01/09/2023
+        Program : Write a program which accept one number from user and toggle 7th and
+                10th bit of that number. Return modified number.  
+
+                Input : 137
+                Output : 713
+    */
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    //Class Declaration
+    class ToggleBit
+    {
+        //Characteiristics of Class
+        public $No;
+
+        //Constructor of Class
+        public function __construct()
+        {
+            $this->No = 0;
+        }
+
+        //Behaviour of Class To Take Input from user
+        public function Accept()
+        {
+            $this->No = 137;
+        }
+
+        //Behaviour of Class To Toggle Bit
+        public function togglebit()
+        {
+            $iResult = 0;           //  0    0    0    0    0    2   4     0
+            $iMask = 0X00000240;   // 0000 0000 0000 0000 0000 0010 0100 0000
+
+            $iResult = $iMask ^ $this->No;
+
+            return $iResult;
+        }
+    }   
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Main function
+    function main() 
+    {
+        $iRet = 0;
+
+        echo "Jay Ganesh......<br>";
+
+        //Creating object of ToggleBit Class
+        $cobj = new ToggleBit();
+
+        //Call To Behaviour of Class
+        $cobj->Accept();
+        $iRet = $cobj->togglebit();
+        
+        //Printing Modified Number 
+        echo "Modified Number is : ",$iRet;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Call To Main function
+    main();
+?>
