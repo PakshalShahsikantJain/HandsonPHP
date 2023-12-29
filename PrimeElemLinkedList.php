0f2b946f669fccf125a5ae2857257521084ccc01
Pakshal <pakshal1256@gmail.com>
2023-12-29 18:09:23 +0530

    commit

---
+++ PrimeElemLinkedList.php
@@ -0,0 +1,136 @@
+<?php
+    class Node 
+    {
+        public $data;
+        public $next;
+
+        public function __construct()
+        {
+            $this->data = 0;
+            $this->next = null;
+        }
+    }
+
+    class SinglyLL
+    {
+        public $Head;
+
+        public function __construct()
+        {
+            $this->Head = null;
+        }
+
+        public function InsertFirst($value)
+        {
+            $newn = new Node();
+            $newn->data = $value;
+            $newn->next = null;
+
+            if($this->Head == null)
+            {
+                $this->Head = $newn;
+            }
+            else
+            {
+                $newn->next = $this->Head;
+                $this->Head = $newn;
+            }            
+        }
+
+        public function InsertLast($value)
+        {
+            $newn = new Node();
+            $newn->data = $value;
+            $newn->next = null;
+            $temp = $this->Head;
+
+            if($this->Head == null)
+            {
+                $this->Head = $newn;
+            }
+            else
+            {
+                while($temp->next != null)
+                {   
+                    $temp = $temp->next;
+                }
+                $temp->next = $newn;
+            }            
+        }
+
+        public function Display()
+        {
+            $Head = $this->Head;
+
+            echo("Elements of linked list are : <br>");
+            while($Head != null)
+            {
+                printf("|%d|=>",$Head->data);
+                $Head = $Head->next;
+            }
+            echo("<br>");
+        }
+
+        public function Count()
+        {
+            $Head = $this->Head;
+            $icnt = 0;
+
+            while($Head != null)
+            {
+                $icnt++;
+                $Head = $Head->next;
+            }
+
+            return $icnt;
+        }
+
+        public function DisplayPrime()
+        {
+            $Head = $this->Head;
+            $isum = 0;
+            $i = 0;
+
+            echo("<br>Prime Elements of linked List are : ");
+            while($Head != null)
+            {
+                for($i = 1;$i <= ($Head->data / 2);$i++)
+                {
+                    if(($Head->data % $i) == 0) 
+                    {
+                        $isum = $isum + 1;
+                    }
+                }
+                if($isum == 1)
+                {
+                    printf("%d &nbsp;&nbsp;",$Head->data);
+                }
+                $isum = 0;
+                $Head = $Head->next;
+            }
+        }
+    }
+
+    function main() 
+    {
+        $iret = 0 ;
+
+        echo "Jay Ganesh....";
+        
+        $sobj = new SinglyLL();
+        $sobj->InsertFirst(51);
+        $sobj->InsertFirst(11);
+        $sobj->InsertFirst(10);
+        $sobj->InsertLast(17);
+        $sobj->InsertLast(41);
+        $sobj->InsertLast(999983);
+
+        $sobj->Display();
+        $iret = $sobj->Count();
+        echo "Count of Elements From linked list is : ",$iret;
+        
+        $sobj->DisplayPrime();
+    }
+
+    main();
+?>
